4884ac26f3b9258bd85bfcb7827517e759999208
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-12-03 20:32:04 -0800

    authmiddleware with only unauthorized errors, to be more consistent

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 import { appEnv } from "../config/env";
-import { ForbiddenError } from "../errors/ForbiddenError";
 import { UnauthorizedError } from "../errors/UnauthorizedError";
 import { User } from "../models/user/user.model";
 import { IRequestCustom } from "../types/express.types";
@@ -18,7 +17,7 @@ export const AuthMiddleware = (req: IRequestCustom, res, next): void => {
       async (err, jwtPayload: any) => {
         if (err) {
           // here we associate the error to a variable because just throwing then inside this async block won't allow them to achieve the outside scope and be caught by errorHandler.middleware. That's why we're passing then to next...
-          const error = new ForbiddenError(
+          const error = new UnauthorizedError(
             "Please, login to access this resource."
           );
           next(error);
@@ -27,7 +26,7 @@ export const AuthMiddleware = (req: IRequestCustom, res, next): void => {
         const dbUser = await User.findOne({ email: jwtPayload.email });
 
         if (!dbUser) {
-          const error = new ForbiddenError(
+          const error = new UnauthorizedError(
             "Please, login to access this resource."
           );
           next(error);
